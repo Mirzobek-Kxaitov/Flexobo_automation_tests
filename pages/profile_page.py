9b175513c0a1b59374b8bb6df7e371117a6e1600
Mirzobek-Kxaitov <mirzobekxaitov@gmail.com>
2026-04-07 22:22:24 +0500

    profile_page

---
+++ pages/profile_page.py
@@ -0,0 +1,68 @@
+from playwright.sync_api import Page, expect
+
+class ProfilePage:
+    def __init__(self, page: Page):
+        self.page = page
+
+    #locators
+        self.sign_in_text = page.get_by_text("Sign In")
+        self.dropdown_menu_trigger = page.locator("button[data-slot='dropdown-menu-trigger']").nth(3)
+        self.profile_item = page.get_by_role("menuitem", name="Profile")
+        self.my_loads_item = page.get_by_role("menuitem", name="My Loads")
+        self.fleet_item = page.get_by_role("menuitem", name="Fleet")
+        self.my_trips_item = page.get_by_role("menuitem", name="My trips")
+        self.logout_item = page.get_by_role("menuitem", name="Logout")
+        self.confirm_yes_button = page.get_by_text("Yes")
+    
+    def open_menu(self):
+        self.sign_in_text.click()
+        self.dropdown_menu_trigger.click()
+        return self
+    
+    def go_to_profile(self):
+        self.open_menu()
+        self.profile_item.click()
+        return self
+    
+    def go_to_my_loads(self):
+        self.open_menu()
+        self.my_loads_item.click()
+        return self
+    
+    def go_to_fleet(self):
+        self.open_menu()
+        self.fleet_item.click()
+        return self
+    
+    def go_to_my_trips(self):
+        self.open_menu()
+        self.my_trips_item.click()
+        return self
+    
+    def logout(self):
+        self.open_menu()
+        self.logout_item.click()
+        self.confirm_yes_button.click()
+        return self
+    
+
+    def expect_on_profile_page(self):
+        expect(self.page).to_have_url("https://app.flexobo-mock.site/profile/root")
+        return self
+    
+    def expect_my_loads_page(self):
+        expect(self.page).to_have_url("https://app.flexobo-mock.site/profile-load")
+        return self
+    
+    def expect_fleet_page(self):
+        expect(self.page).to_have_url("https://app.flexobo-mock.site/tms/fleet")
+        return self
+
+    def expect_my_trips_page(self):
+        expect(self.page).to_have_url("https://app.flexobo-mock.site/profile-trips")
+        return self
+
+    def expect_logged_out(self):
+        expect(self.page).to_have_url("https://landing-dev.flexobo.com/", timeout=10000)
+        return self
+
--- tests/test_profile.py
+++ tests/test_profile.py
@@ -1,40 +1,17 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
+from pages.profile_page import ProfilePage
 
-def open_menu(page: Page):
-    """Dropdown menyu ochish"""
-    page.get_by_text("Sign In").click()
-    page.locator("button[data-slot='dropdown-menu-trigger']").nth(3).click()
-
-def test_open_profile_page(logged_in: Page):
-    page = logged_in
-    open_menu(page)
-    page.get_by_role("menuitem", name="Profile").click()
-    expect(page).to_have_url("https://app.flexobo-mock.site/profile/root")
+def test_profile_page(logged_in: Page):
+    ProfilePage(logged_in).go_to_profile().expect_on_profile_page()
 
 def test_my_loads_page(logged_in: Page):
-    page = logged_in
-    open_menu(page)
-    page.get_by_role("menuitem", name="My Loads").click()
-    expect(page).to_have_url("https://app.flexobo-mock.site/profile-load")
+    ProfilePage(logged_in).go_to_my_loads().expect_my_loads_page()
 
 def test_my_trucks_page(logged_in: Page):
-    page = logged_in
-    open_menu(page)
-    page.get_by_role("menuitem", name="Fleet").click()
-    expect(page).to_have_url("https://app.flexobo-mock.site/tms/fleet")
+    ProfilePage(logged_in).go_to_fleet().expect_fleet_page()
 
 def test_my_trips_page(logged_in: Page):
-    page = logged_in
-    open_menu(page)
-    page.get_by_role("menuitem", name="My trips").click()
-    expect(page).to_have_url("https://app.flexobo-mock.site/profile-trips")
+    ProfilePage(logged_in).go_to_my_trips().expect_my_trips_page()
 
 def test_user_can_logout(logged_in: Page):
-    page = logged_in
-    accept_button = page.get_by_role("button", name="Accept")
-    if accept_button.is_visible():
-        accept_button.click()
-    open_menu(page)
-    page.get_by_role("menuitem", name="Logout").click()
-    page.get_by_text("Yes").click()
-    expect(page).to_have_url("https://landing-dev.flexobo.com/", timeout=10000)
+    ProfilePage(logged_in).logout().expect_logged_out()
